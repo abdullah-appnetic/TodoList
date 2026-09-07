620274138bef14b79ce6be7bae891fafe437786b
abdullah-appnetic <abdullah.appnetic@gmail.com>
2026-09-07 14:40:26 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dev Internee 1 on 07/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
